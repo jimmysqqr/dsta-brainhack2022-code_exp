ac7f8b8cc82f9ef5e27d4b5b1126f7bb7dc81a57
jimmysqqr <77312754+jimmysqqr@users.noreply.github.com>
2022-06-10 22:32:10 +0800

    init

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>DSTA brainhack 2022 CODE_EXP</Text>
       <StatusBar style="auto" />
     </View>
   );
